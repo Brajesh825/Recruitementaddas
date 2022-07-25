2d7b00f5c997c552ce490b663da8b1a1fa547e13
Brajesh Mishra <brajeshmishra825@gmail.com>
2022-07-25 13:30:05 +0530

    :p

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use("/video/", video);
 const start = async() => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(4000, console.log(`Server is listening on port ${4000}...`));
+        app.listen(process.env.PORT, console.log(`Server is listening on port ${4000}...`));
     } catch (error) {
         console.log(error);
     }
