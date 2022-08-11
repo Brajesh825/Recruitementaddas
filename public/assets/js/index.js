618b6747d9963f4f0fa1f0510d64adb39e7cdebb
Brajesh Mishra <brajeshmishra825@gmail.com>
2022-08-11 14:06:20 +0530

    Updated TEAM MEMBERS

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,3 +7,16 @@ function NavBar() {
         x.className = "topnav";
     }
 }
+
+const navItems = document.querySelectorAll(".nav__item")
+
+navItems.forEach((item)=>{
+    item.addEventListener('click' , (e)=>{
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    })
+})
